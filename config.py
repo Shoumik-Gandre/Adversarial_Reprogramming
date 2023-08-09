85a22ac2b3f38e32392466597c929e36a31314d9
Shoumik Gandre <61053611+Shoumik-Gandre@users.noreply.github.com>
2023-08-09 16:23:55 -0700

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ cfg.train_dir = 'train_log'
 cfg.models_dir = 'models'
 cfg.data_dir = 'datasets'
 
-cfg.batch_size_per_gpu = 30
+cfg.batch_size_per_gpu = 50 # We use 2 gpus
 cfg.w1 = 224
 cfg.h1 = 224
 cfg.w2 = 28
 cfg.h2 = 28
-cfg.lmd = 5e-7
+cfg.lmd = 5e-3
 cfg.lr = 0.05
 cfg.decay = 0.96
-cfg.max_epoch = 30
+cfg.max_epoch = 100
 
 if not os.path.exists(cfg.train_dir):
     os.makedirs(cfg.train_dir)
